442cae4c8ae84c236c4771c944479610deef4aeb
Natalia <Natalia.Terlecka@gmail.com>
2012-06-21 22:11:27 +0200

    commit

---
+++ HackKrk6/leaderbords.h
@@ -0,0 +1,13 @@
+//
+//  leaderbords.h
+//  HackKrk6
+//
+//  Created by Natalia Terlecka on 6/21/12.
+//  Copyright (c) 2012 AGH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface leaderbords : UITableViewController
+
+@end
